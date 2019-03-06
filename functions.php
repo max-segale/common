32be51603453c10ec50136c7ecd83592f08d5911
Max <maxsegale@gmail.com>
2019-03-05 19:59:29 -0500

    new library

---
+++ functions.php
@@ -0,0 +1,20 @@
+<?php
+
+function sqlQuery($query) {
+    global $sqlHost, $sqlUser, $sqlPass, $dbName;
+    $mysqli = new mysqli($sqlHost, $sqlUser, $sqlPass, $dbName);
+    if (mysqli_connect_errno()) {
+        printf("Connect failed: %s\n", mysqli_connect_error());
+        exit();
+    }
+    if ($result = $mysqli->query($query)) {
+        return $result;
+    }
+    $mysqli->close();
+}
+
+function dateTimeStr($dateTime) {
+    return $dateTime->format('Y-m-d H:i:s');
+}
+
+?>

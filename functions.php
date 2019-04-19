7f6d846bb31daeecc316e99ebb0d14da6673726d
Max <maxsegale@gmail.com>
2019-04-19 10:04:54 -0400

    css vendor prefixes

--- functions.php
+++ functions.php
@@ -1,5 +1,6 @@
 <?php
 
+// mysql query
 function sqlQuery($query) {
     global $sqlHost, $sqlUser, $sqlPass, $dbName;
     $mysqli = new mysqli($sqlHost, $sqlUser, $sqlPass, $dbName);
@@ -13,8 +14,30 @@ function sqlQuery($query) {
     $mysqli->close();
 }
 
+// return datetime object as string
 function dateTimeStr($dateTime) {
     return $dateTime->format('Y-m-d H:i:s');
+}
+
+// add full vendor prefixes to a css property or value
+function vFix($property, $value, $toValue) {
+    $vendors = ['-webkit-', '-moz-', '-ms-'];
+    foreach ($vendors as $v) {
+        $propCSS .= "$v$property: ";
+        if ($toValue) {
+            $propCSS .= $v;
+        }
+        $propCSS .= "$value;
+    ";
+    }
+    if ($toValue) {
+        foreach ($vendors as $v) {
+            $propCSS .= "$property: $v$value;
+    ";
+        }
+    }
+    $propCSS .= "$property: $value;";
+    return $propCSS;
 }
 
 ?>
